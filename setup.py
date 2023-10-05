56e8d17af5798cb16c680c94edc69b2582be9f05
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-10-05 15:10:54 +0200

    [setup] support django >= 2.2

--- setup.py
+++ setup.py
@@ -46,6 +46,9 @@ setup(
     classifiers=[
         dev_status,
         "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.1",
